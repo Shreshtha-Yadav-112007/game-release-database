a6b8a2cbc36d469e4ee44e9dec9fff8570c352ce
ssjthegoat <Shreshthayadav11@gmail.com>
2026-08-25 17:02:06 +0530

    feat: set up React frontend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
+const cors = require("cors");
 const express = require("express");
 const pool = require("./db");
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Game Release API is running!");
